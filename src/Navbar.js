3a11a988431a4d3d14120830467df035e217b781
Haris Ibragic <ibragicharis01@gmail.com>
2024-05-05 00:21:36 +0200

    added links

--- src/Navbar.js
+++ src/Navbar.js
@@ -58,3 +58,40 @@
 // };
 
 // export default Navbar;
+
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' className='logo' />
+          <button className='nav-toggle' onClick={toggleLinks}></button>
+        </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map(link => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>{}</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
